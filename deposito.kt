2bcc55b766a286689680b88450c48db1ec2fbe56
aleff959 <aleff959@gmail.com>
2025-11-19 13:04:06 -0300

    v3.0.0

--- deposito.kt
+++ deposito.kt
@@ -8,25 +8,32 @@ fun deposito() {
   """)
   println("----------------------------")
 
-  val adcSaldo: Double? = readln().trim()
+  val entradaDeposito = readln().trim()
+  val adcSaldo: Double? = entradaDeposito.toDoubleOrNull()
   
-  
-  if (adcSaldo != null || adcSaldo >=0.01) {
+  if (adcSaldo != null && adcSaldo >=0.01) {
     
     println("Adicionando saldo à sua conta...")
     saldo += adcSaldo
     println("""
     Saldo adicionado!!!
-    ---Seu saldo atual é: R$ {"%.2f.format(saldo)}---
+    ---Seu saldo atual é: R$ ${"%.2f.format(saldo)"}---
     """)
     println("--------------------------")
     println("Voltando ao menu principal...")
-    break
+    return
     
   } else if (adcSaldo != null && adcSaldo <= 0.0) {
     
     println("É necessário adicionar um valor acima de: R$ 0.0")
-    println("")
+    println("Voltando ao menu principal")
+    return
+    
+  } else {
+    
+    println("Erro! Por favor, tente novamente.")
+    println("Voltando ao menu principal")
+    return
     
   }
 }
