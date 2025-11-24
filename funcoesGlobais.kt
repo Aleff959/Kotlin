bf602b59b0a8ce6d94f8852fd319f328b32b2b3c
aleff959 <aleff959@gmail.com>
2025-11-24 11:38:16 -0300

    Adição de funções de separadores: bordas finais e iniciais.

--- funcoesGlobais.kt
+++ funcoesGlobais.kt
@@ -1,6 +1,16 @@
 var saldo: Double = 0.0
 
-fun imprimirCabecalho(titulo: String) {
-    println("\n---$titulo---")
+fun imprimirCabecalho(titulo: String){
+    println("\n---------$titulo---------")
     println("----------------------------") 
+}
+
+fun imprimirExtrato(titulo: String){
+    println("=======================================")
+    println("\n----$titulo----")
+    println("--------------------------")
+}
+
+fun bordas() {
+    println("=======================================")
 }
